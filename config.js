9f0c35911effe3d2198ad37aa4b0c7c09b8aad92
John <insideoutdevco@gmail.com>
2021-06-10 18:01:37 -0700

    partial code cleanup

--- config.js
+++ config.js
@@ -8,8 +8,7 @@ const config = {
     user: env.DB_USER,
     password: env.DB_PASSWORD,
     database: env.DB_NAME,
-  },
-  listPerPage: env.LIST_PER_PAGE || 10,
+  }
 };
 
 
--- services/tasks.js
+++ services/tasks.js
@@ -21,10 +21,10 @@ async function getAll(page = 1, limit = 1, topic, sortBy='inserted', order='ASC'
   let params = [];
 
   if(topic) {
-    query = `SELECT task_id, topic, inserted, priority  FROM tasks WHERE topic LIKE ? ORDER BY ${sortBy} ${order} LIMIT ?,?`;
+    query = `SELECT topic, inserted, priority  FROM tasks WHERE topic LIKE ? ORDER BY ${sortBy} ${order} LIMIT ?,?`;
     params = [topic, offset, limit]
   } else {
-    query = `SELECT task_id, topic, inserted, priority  FROM tasks ORDER BY ${sortBy} ${order} LIMIT ?,?`;
+    query = `SELECT topic, inserted, priority  FROM tasks ORDER BY ${sortBy} ${order} LIMIT ?,?`;
     params = [offset, limit]
   }
 
